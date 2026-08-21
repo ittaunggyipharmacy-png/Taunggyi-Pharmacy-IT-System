c6f41bb3a9e5cf06c0a23c505f9a9556ceaaf13f
IT TGP <it.taunggyipharmacy@gmail.com>
2026-08-21 09:52:27 +0630

    Delete test_regex2.cjs

--- test_regex2.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-let app = fs.readFileSync('src/App.tsx', 'utf8');
-
-const p1 = app.match(/\s*useEffect\(\(\) => \{\s*if \(isAdmin\) \{\s*const loadAndMigratePasswords.*?loadAndMigratePasswords\(\);\s*\}\s*\}, \[isAdmin, settings, setSettings\]\);/s);
-console.log("Match 1:", !!p1);
-
-const p4 = app.match(/\s*\{\/\* RESET DATABASE TOOL \*\/\}.*?<\/ResetAssetsButton>\n\s*<\/div>\n\s*\)}/s);
-console.log("Match 4:", !!p4);
