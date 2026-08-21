865e1271329a4fff74c159abefe16740e54363de
IT TGP <it.taunggyipharmacy@gmail.com>
2026-08-21 09:52:43 +0630

    Delete test_regex3.cjs

--- test_regex3.cjs
+++
@@ -1,4 +0,0 @@
-const fs = require('fs');
-let app = fs.readFileSync('src/App.tsx', 'utf8');
-const p4 = app.match(/\s*\{\/\* RESET DATABASE TOOL \*\/\}.*?isCompact=\{true\} \/>\n\s*<\/div>\n\s*\)}/s);
-console.log("Match 4:", !!p4);
