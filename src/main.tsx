85a61bbdbfa547ab8b85af0a0e2b89c90b779a95
IT TGP <it.taunggyipharmacy@gmail.com>
2026-08-24 17:08:17 +0630

    feat: enable Assets by User department filter

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,11 @@ import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { AccessControlProvider } from './contexts/AccessControlContext';
 import { installAssetAssignmentUiEnhancer } from './features/assets/assignmentUiEnhancer';
+import { installAssetsByUserDepartmentFilter } from './features/assets/assetsByUserDepartmentEnhancer';
 import './index.css';
 
 installAssetAssignmentUiEnhancer();
+installAssetsByUserDepartmentFilter();
 
 createRoot(document.getElementById('root')!).render(
  <StrictMode>
