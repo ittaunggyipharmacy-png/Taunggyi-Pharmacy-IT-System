cf416af6a5cc923a6953c3ca1b663b185bbeced0
IT TGP <it.taunggyipharmacy@gmail.com>
2026-08-24 14:56:35 +0630

    fix(security): correct SecurityCombined export import

--- src/features/security/SecurityCombined.tsx
+++ src/features/security/SecurityCombined.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { SecurityPage } from './SecurityPage';
+import { SecurityModule } from './SecurityPage';
 import { InfrastructurePage } from './InfrastructurePage';
 import { BackupLog, CCTVRequest } from '../../types';
 
@@ -13,7 +13,7 @@ export function SecurityCombined(props: {
 }) {
   return (
     <div className="space-y-10">
-      <SecurityPage {...props} />
+      <SecurityModule {...props} />
       <div className="border-t border-slate-200 dark:border-slate-800 pt-8">
         <InfrastructurePage isAdmin={props.isAdmin} />
       </div>
