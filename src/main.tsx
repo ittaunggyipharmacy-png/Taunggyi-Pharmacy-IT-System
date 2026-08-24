b5675dde1c052b86d0170f2e7cc52dfefd0ea942
IT TGP <it.taunggyipharmacy@gmail.com>
2026-08-24 16:47:48 +0630

    feat: enable searchable asset assignment UI

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,10 @@ import {StrictMode} from 'react';
 import {createRoot} from 'react-dom/client';
 import App from './App.tsx';
 import { AccessControlProvider } from './contexts/AccessControlContext';
+import { installAssetAssignmentUiEnhancer } from './features/assets/assignmentUiEnhancer';
 import './index.css';
+
+installAssetAssignmentUiEnhancer();
 
 createRoot(document.getElementById('root')!).render(
  <StrictMode>
